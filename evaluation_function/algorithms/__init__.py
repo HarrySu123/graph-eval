232806b55b574e9e14bc6593d063bae8b17b65e7
Johnny Wan <2695191695@qq.com>
2026-01-30 02:47:05 +0000

    Add cycle detection parameters and include cycle info in algorithms
    - Added `max_nodes` and `max_cycles` fields to `CycleDetectionParams` for enhanced cycle enumeration control.
    - Included `cycle_info` in the algorithms module to support cycle detection functionality.

--- evaluation_function/algorithms/__init__.py
+++ evaluation_function/algorithms/__init__.py
@@ -5,10 +5,12 @@ Core graph algorithms used by the evaluation function.
 from .connectivity import connectivity_info
 from .shortest_path import shortest_path_info
 from .bipartite import bipartite_info
+from .cycles import cycle_info
 
 __all__ = [
     "connectivity_info",
     "shortest_path_info",
     "bipartite_info",
+    "cycle_info",
 ]
 
